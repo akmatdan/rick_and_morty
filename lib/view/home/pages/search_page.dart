5c2e5fe532d12c4d1ac49728b4f9607e77a0ff31
Dan Akmat <akmatdan@gmail.com>
2023-11-22 23:55:25 +0600

    Created supporting vars and ListView

--- lib/view/home/pages/search_page.dart
+++ lib/view/home/pages/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_rick_and_morty/bloc/character_bloc.dart';
+import 'package:the_rick_and_morty/data/models/character.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -10,11 +11,19 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  // Supporting vars
+  late Character _currentCharacter;
+  List<Results> _currentResults = [];
+  int _currentPage = 1;
+  String _currentSearchStr = '';
+
   @override
   void initState() {
-    context
-        .read<CharacterBloc>()
-        .add(const CharacterEvent.fetch(name: '', page: 1));
+    if (_currentResults.isEmpty) {
+      context
+          .read<CharacterBloc>()
+          .add(const CharacterEvent.fetch(name: '', page: 1));
+    }
     super.initState();
   }
 
@@ -41,14 +50,24 @@ class _SearchPageState extends State<SearchPage> {
               hintText: 'Search name',
               hintStyle: const TextStyle(color: Colors.grey),
             ),
+
+            // Method for TextField
             onChanged: (value) {
+              _currentPage = 1;
+              _currentResults = [];
+              _currentSearchStr = value;
+
               context
                   .read<CharacterBloc>()
-                  .add(CharacterEvent.fetch(name: value, page: 1));
+                  .add(CharacterEvent.fetch(name: value, page: _currentPage));
             },
           ),
         ),
-        state.when(
+
+        // States
+        Expanded(
+          child: state.when(
+            // Loading state
             loading: () {
               return const Center(
                 child:
@@ -59,9 +78,38 @@ class _SearchPageState extends State<SearchPage> {
                 ]),
               );
             },
-            loaded: (characterLoaded) => Text('${characterLoaded.info}'),
-            error: () => const Text('Nothing found...')),
+
+            // Loaded state
+            loaded: (characterLoaded) {
+              _currentCharacter = characterLoaded;
+              _currentResults = _currentCharacter.results;
+              return _currentResults.isNotEmpty
+                  ? Text('$_currentResults')
+                  : const SizedBox();
+            },
+
+            // Error state
+            error: () => const Text('Nothing found...'),
+          ),
+        ),
       ],
+    );
+  }
+
+  Widget _customListView(List<Results> currentResults) {
+    return ListView.separated(
+      itemCount: currentResults.length,
+      separatorBuilder: (_, index) => const SizedBox(height: 5),
+      shrinkWrap: true, // takes neccesary space depend on element
+      itemBuilder: (context, index) {
+        final result = currentResults[index];
+        return ListTile(
+          title: Text(
+            result.name,
+            style: const TextStyle(color: Colors.white),
+          ),
+        );
+      },
     );
   }
 }
